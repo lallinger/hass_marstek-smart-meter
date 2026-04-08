aa4f77d937355c59cb24bfecfa9c14502fe4debd
lallinger <32633433+lallinger@users.noreply.github.com>
2026-04-08 17:52:26 +0200

    Set 2s Update interval

--- custom_components/marstek_ct/__init__.py
+++ custom_components/marstek_ct/__init__.py
@@ -38,7 +38,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         _LOGGER,
         name="marstek_ct_sensor",
         update_method=async_update_data,
-        update_interval=timedelta(seconds=30),
+        update_interval=timedelta(seconds=2),
     )
 
     await coordinator.async_config_entry_first_refresh()
